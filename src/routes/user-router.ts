0781301127c3b99434dc32ff14c68f3d7fb6b755
Marcos Lima <mrsk1414@gmail.com>
2026-01-08 20:12:39 -0300

    feat: add schema validation in router

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginUser, signUp } from "../controllers/user-controller";
+import { validateSchema } from "middlewares/schema.middleware";
+import { loginSchema } from "schemas/index-schemas";
 
 const userRouter = Router();
 
 userRouter.post("/sign-up", signUp);
-userRouter.post("/sign-in", loginUser);
+userRouter.post("/sign-in", validateSchema(loginSchema), loginUser);
 
 export default userRouter;
