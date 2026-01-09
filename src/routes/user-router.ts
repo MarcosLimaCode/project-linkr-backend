1eabe2bcdbe0b267253cd9b3735f150d8a475890
Marcos Lima <mrsk1414@gmail.com>
2026-01-08 22:39:48 -0300

    fix: add login input validation schema

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { loginUser, signUp } from "../controllers/user-controller";
-import { validateSchema } from "middlewares/schema.middleware";
-import { loginSchema } from "schemas/index-schemas";
+import { validateSchema } from "../middlewares/schema-middleware";
+import { loginSchema, signUpSchema } from "../schemas/index-schemas";
 
 const userRouter = Router();
 
-userRouter.post("/sign-up", signUp);
-userRouter.post("/sign-in", validateSchema(loginSchema), loginUser);
+userRouter.post("/", validateSchema(loginSchema), loginUser);
+userRouter.post("/sign-up", validateSchema(signUpSchema), signUp);
 
 export default userRouter;
