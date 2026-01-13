0e23b7358243dc9bcbb5d898bb34a5b313019682
Jorge Koch <jorgeluizkoch@gmail.com>
2026-01-13 00:52:24 -0300

    refactor: streamline loginUserServices function and improve password handling
    refactor: streamline loginUserServices function and improve password handling

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -47,18 +47,13 @@ export async function createUserService({
   });
 }
 
-
 export async function loginUserServices(req: signInProtocol) {
   const foundEmail = await verifyEmailRepository(req.email);
   if (!foundEmail) throw notFound("Email não encontrado.");
-
-  const password = req.password.trim();
-
-  const checkPassword = bcrypt.compareSync(password, foundEmail.password);
-  if (!checkPassword) throw unauthorizedError("Senha incorreta.");
-
   const token = jwt.sign(
-    { userId: foundEmail.id },
+    {
+      userId: foundEmail.id,
+    },
     process.env.JWT_SECRET,
     { expiresIn: 86400 }
   );
@@ -66,6 +61,5 @@ export async function loginUserServices(req: signInProtocol) {
   const checkPassword = bcrypt.compareSync(req.password, foundEmail.password);
   if (!checkPassword) throw unauthorizedError("Senha incorreta.");
 
-  return token;
+  return { token, image: foundEmail.image };
 }
-
