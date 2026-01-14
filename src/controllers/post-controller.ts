23e26805b2a104cce24d6981fb3dec0e68b8d9b8
Marcos Lima <mrsk1414@gmail.com>
2026-01-14 16:30:43 -0300

    fix: update userId reference in createPost controller and add userId to post protocol

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -3,7 +3,7 @@ import httpStatus from "http-status";
 import { createPostServices } from "../services/post-service";
 
 export async function createPost(req: Request, res: Response) {
-  const userId = res.locals.user;
+  const userId = res.locals.userId;
   await createPostServices(req.body, userId);
   return res.sendStatus(httpStatus.CREATED);
 }
