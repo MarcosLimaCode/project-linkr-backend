4913ba361c89df258c272400c6bcef948fdce37e
Marcos Lima <mrsk1414@gmail.com>
2026-01-14 16:56:02 -0300

    feat: add delete post functionality with controller, service, and repository updates

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
-import { createPostServices } from "../services/post-service";
+import {
+  createPostServices,
+  deletePostServices,
+} from "../services/post-service";
 
 export async function createPost(req: Request, res: Response) {
   const userId = res.locals.userId;
   await createPostServices(req.body, userId);
   return res.sendStatus(httpStatus.CREATED);
+}
+
+export async function deletePost(req: Request, res: Response) {
+  const id = Number(req.params.id);
+  await deletePostServices(id);
+  res.sendStatus(204);
+  return;
 }
