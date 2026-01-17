84c2639f8998c28184727603b4faa672f0288a11
Jorge Koch <jorgeluizkoch@gmail.com>
2026-01-17 19:29:03 -0300

    my profile page info loaded

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -29,10 +29,29 @@ export async function findUser(req: Request, res: Response) {
   return res.status(httpStatus.OK).send(result);
 }
 
-export async function getMyProfile(req: Request, res: Response) {
-  // Observar o que o Frontend precisa. Mas a base está realizada.
 
-  const id = Number(res.locals.userId);
-  const result = await findUserService(id);
-  return res.status(httpStatus.OK).send(result);
+export async function getMyProfile(req: Request, res: Response) {
+  try {
+    const id = Number(res.locals.userId);
+
+    if (!id) {
+      return res.status(httpStatus.UNAUTHORIZED).send({
+        message: "Usuário não autenticado",
+      });
+    }
+
+    const { user } = await findUserService(id);
+
+    return res.status(httpStatus.OK).send({
+      name: user.username,
+      age: user.age,
+      image: user.image,
+      about: user.about,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+      message: "Erro ao buscar perfil do usuário",
+    });
+  }
 }
